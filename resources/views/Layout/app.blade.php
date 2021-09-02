5d1612200b5624d49cba45d32094ab327a6a2c3f
Vahan Niazyan <60444426+VahanNiazyan@users.noreply.github.com>
2021-09-02 10:39:59 +0400

    first project

---
+++ resources/views/Layout/app.blade.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+</head>
+<body>
+<header>
+    <div class="header-part">
+        <div class="header-img">
+            <img src="/images/header.jpg">
+        </div>
+        <div class="d-flex">
+            <div class="header-home">
+                <ul>
+                    <li><a href="#">Գլխավոր</a></li>
+                    <li><a href="#">Մեր մասին</a></li>
+                    <li><a href="#">Նախագծեր</a></li>
+                    <li><a href="#">Ծառայություններ</a></li>
+                    <li><a href="#">Գործնկերներ</a></li>
+                    <li><a href="#">Կոնտակտներ</a></li>
+                </ul>
+            </div>
+            <div class="header-language">
+                <ul>
+                    <li><a href="#">Հայ</a></li>
+                    <li><a href="#">Рус</a></li>
+                    <li><a href="#">Eng</a></li>
+                    <li class="contact"><a href="#"><i class="fa fa-phone"></i></a></li>
+                    <li class="contact"><a href="#"><i class="fa fa-map-marker"></i></a></li>
+                </ul>
+            </div>
+            <div>
+            </div>
+        </div>
+    </div>
+
+    <div class="header-main-images">
+        <div class="header-color">
+            <div class="header-flex container">
+                <div class="header-text">
+                    <h2>Roof Garden</h2>
+                    <h5>Ճարտարապետություն և Շինարարություն</h5>
+                    <p>Ցանկացած տեսակի տարածքի նախագծերի մշակում
+                        կարճ ժամանակահատվածում</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<section class="container second-section">
+    <div class="row">
+        <div class="second-section-text col-6">
+            <p class="first-p">Մեր մասին</p>
+            <h2>Roof Garden</h2>
+            <h6>Հաճախորդների վրա կենտրոնացած ճարտարապետության և շինարարության լավագույն ծառայությունների մատուցում:</h6>
+            <p class="second-p">Չնայած մեր ընկերության համեմատաբար երիտասարդ լինելուն, մեզ հաջողվել է համախմբել ոլորտի
+                փորձառու մասնագետներին և ստանալ լավագույն թիմը, որը սիրով կյանքի կկոչի Ձեր ամենահամարձակ երազանքները։
+                Հիմնվելով որակի ամենաբարձր ստանդարտների վրա, մենք կձեռբազատենք Ձեզ նյարդերի և ժամանակի անտեղի
+                վատնումից։ </p>
+            <button><a>Կարդալ ավելին</a></button>
+        </div>
+        <div class="second-section-img col-6">
+            <img src="/images/Group_6014.jpg">
+        </div>
+    </div>
+</section>
+
+<section class="section-services container">
+    <h2>Մեր ծառայությունները</h2>
+    <div class="row section-services-flex">
+        <div class="images-first col-3">
+            <img src="/images/Group 6034.png" alt="No images">
+        </div>
+        <div class="images-second col-3">
+            <img src="/images/Group 6084.png" alt="No images">
+        </div>
+        <div class="images-thirty col-3">
+            <img src="/images/Group 6033.png" alt="No images">
+        </div>
+        <div class="images-fourth col-3">
+            <img src="/images/Group 6032.png" alt="No images">
+        </div>
+    </div>
+    <div>
+
+    </div>
+</section>
+</body>
+</html>
